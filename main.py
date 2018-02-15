417b097e03fff0e092093f16c90ade61d17dc8fb
Jérémy COHEN <j.cohen@LGOP1661.local>
2018-02-15 16:27:26 +0100

    Modify load_vgg

--- main.py
+++ main.py
@@ -32,8 +32,16 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    tf.saved_model.loader.load(sess,[vgg_tag],vgg_path)
+
+    graph = tf.get_default_graph()
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    w3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    w4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    w7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    return w1, keep, w3, w4, w7
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,6 +55,19 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
+    # 1x1 CONVOLUTION TO PRESERVE SPATIAL INFORMATION
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out,num_classes,1,strides=(1,1),padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    # UPSAMPLING TO ORIGINAL IMAGE SIZE
+    layer7_1 = tf.layers.conv2d_transpose(conv_1x1,num_classes,4,strides=(2,2),padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    
+    upsampled = tf.layers.conv2d_transpose(layer7_1,num_classes,4,strides=(2,2),padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    
     return None
 tests.test_layers(layers)
 
